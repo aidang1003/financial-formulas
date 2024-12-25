29bfa3239f4fe0b9de90ba13f1eadd15a5393a3a
Aidan Gorny <aidang1003@gmail.com>
2024-12-24 21:07:58 -0600

    wallet address

--- finance/auth.py
+++ finance/auth.py
@@ -13,23 +13,23 @@ auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 @auth_bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        username = request.form['username']
+        walletaddress = request.form['walletaddress']
         db = get_db()
         error = None
 
-        if not username:
-            error = 'Username is required.'
+        if not walletaddress:
+            error = 'Wallet address is required.'
 
 
         if error is None:
             try:
                 db.execute(
                     "INSERT INTO user (walletaddress, minimumBalance, maximumBalance, factor) VALUES (?, ?, ?, ?)",
-                    (username, 0, 1000, 2), # hard-coded values for now #TODO
+                    (walletaddress, 0, 5000, 2), # hard-coded values for now #TODO
                 )
                 db.commit()
             except db.IntegrityError:
-                error = f"User {username} is already registered."
+                error = f"Address {walletaddress} is already registered."
             else:
                 return redirect(url_for("auth.login"))
 
@@ -40,17 +40,15 @@ def register():
 @auth_bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        walletaddress = request.form['walletaddress']
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
+            'SELECT * FROM user WHERE walletaddress = ?', (walletaddress,)
         ).fetchone()
 
         if user is None:
-            error = 'Incorrect username.'
-        #elif not check_password_hash(user['password'], password):
-        #    error = 'Incorrect password.'
+            error = 'Incorrect Wallet Address.'
 
         if error is None:
             session.clear()
@@ -60,3 +58,30 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+@auth_bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@auth_bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
